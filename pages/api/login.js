2b1a2e17c2ce788589125072395bfa4557a73f2f
王瑞瑜 <roywang@h2o-team.com>
2021-04-27 21:07:52 +0800

    add login functionality

--- pages/api/login.js
+++ pages/api/login.js
@@ -22,11 +22,10 @@ export default nextConnect()
   .post(async (req, res) => {
     try {
       const user = await authenticate('local', req, res)
-      // session is the payload to save in the token, it may contain basic info about the user
-      console.log('login/user:', user)
-      // const session = { ...user }
 
-      // await setLoginSession(res, session)
+      // session is the payload to save in the token, it may contain basic info about the user
+      const session = { ...user }
+      await setLoginSession(res, session)
 
       res.status(200).send({ done: true })
     } catch (error) {
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { useUser } from '../lib/hooks'
 
 export default function Home (props) {
+  const user = useUser({ redirectTo: '/login' })
+  console.log('user:', user)
   return (
     <div>
       <main>
         index page
+        currently logged in as { user?.user_name }
       </main>
     </div>
   )
