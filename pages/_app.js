8e2ccdd4c2d85b11c655f593e8044b526b7d5fc1
王瑞瑜 <roywang@h2o-team.com>
2021-04-28 17:47:35 +0800

    remove redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import 'tailwindcss/tailwind.css'
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
 import Link from 'next/link'
 
 export default function App ({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <div>
       <Link href='/'>Account</Link>
       <Component {...pageProps} />
-    </Provider>)
+    </div>)
 }
