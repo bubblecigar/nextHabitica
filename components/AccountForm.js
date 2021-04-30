90adcef9a2c735672fcd8e1d8ff8206262d74831
王瑞瑜 <roywang@h2o-team.com>
2021-04-30 16:19:28 +0800

    Refactor error message and lint <AccountForm />

--- components/AccountForm.js
+++ components/AccountForm.js
@@ -15,7 +15,7 @@ export const login = async body => {
     }
   } catch (error) {
     console.log('login error:', error)
-    return error
+    return error.message
   }
 }
 
@@ -33,7 +33,7 @@ export const signup = async body => {
     }
   } catch (error) {
     console.error('signup error:', error)
-    return error
+    return 'Sign up error, username might be used'
   }
 }
 
@@ -48,54 +48,54 @@ export default function AccountForm (props) {
       password: e.currentTarget.password.value
     }
     if (submitType === 'login') {
-      const err = await login(body)
-      setErrorMsg(err?.message)
+      const errMsg = await login(body)
+      setErrorMsg(errMsg)
     } else if (submitType === 'signup') {
-      const err = await signup(body)
-      setErrorMsg(err?.message)
+      const errMsg = await signup(body)
+      console.log('errMsg:', errMsg)
+      setErrorMsg(errMsg)
     }
   }
 
   return (
-    <div className="max-w-md w-full space-y-8">
-      <div>{errorMsg}</div>
+    <div className='max-w-md w-full space-y-8'>
       <div>
         <img
-          className="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          alt="Workflow"
+          className='mx-auto h-12 w-auto'
+          src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+          alt='Workflow'
         />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Who are you?</h2>
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Who are you?</h2>
       </div>
-      <form id='account-form' className="mt-8 space-y-6" onSubmit={handleSubmit}>
-        <input type="hidden" name="remember" defaultValue="true" />
-        <div className="rounded-md shadow-sm -space-y-px">
+      <form id='account-form' className='mt-8 space-y-6' onSubmit={handleSubmit}>
+        <input type='hidden' name='remember' defaultValue='true' />
+        <div className='rounded-md shadow-sm -space-y-px'>
           <div>
-            <label htmlFor="username" className="sr-only">
+            <label htmlFor='username' className='sr-only'>
               User name
             </label>
             <input
-              id="username"
-              name="username"
-              type="text"
-              autoComplete="text"
+              id='username'
+              name='username'
+              type='text'
+              autoComplete='text'
               required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="User name"
+              className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+              placeholder='User name'
             />
           </div>
           <div>
-            <label htmlFor="password" className="sr-only">
+            <label htmlFor='password' className='sr-only'>
               Password
             </label>
             <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
+              id='password'
+              name='password'
+              type='password'
+              autoComplete='current-password'
               required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
+              className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+              placeholder='Password'
             />
           </div>
         </div>
@@ -121,25 +121,26 @@ export default function AccountForm (props) {
         </div> */}
         <div>
           <button
-            type="submit"
+            type='submit'
             form='account-form'
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
             onClick={() => setSubmitType('login')}
           >
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-              <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
+              <LockClosedIcon className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400' aria-hidden='true' />
             </span>
             Log in
           </button>
           <button
-            type="submit"
+            type='submit'
             form='account-form'
-            className="my-2 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className='my-2 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
             onClick={() => setSubmitType('signup')}
           >
             Sign up
           </button>
         </div>
+        <div>&nbsp;{errorMsg}</div>
       </form>
     </div>
   )
