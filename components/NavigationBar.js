08a98eb57e472b323d89e2deec922b9fd611ed7e
王瑞瑜 <roywang@h2o-team.com>
2021-05-01 19:57:09 +0800

    add index router button

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -28,11 +28,13 @@ const NavigationBar = () => {
               <div className='flex items-center justify-between h-16'>
                 <div className='flex items-center'>
                   <div className='flex-shrink-0'>
-                    <img
-                      className='h-8 w-8'
-                      src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
-                      alt='Workflow'
-                    />
+                    <Link href='/'>
+                      <img
+                        className='h-8 w-8 cursor-pointer'
+                        src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
+                        alt='Workflow'
+                      />
+                    </Link>
                   </div>
                   <div className='hidden md:block'>
                     <div className='ml-10 flex items-baseline space-x-4'>
@@ -120,7 +122,6 @@ const NavigationBar = () => {
                 {navigation.map((item, itemIdx) =>
                   itemIdx === 0 ? (
                     <Fragment key={item}>
-                      {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                       <a href='#' className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>
                         {item}
                       </a>
