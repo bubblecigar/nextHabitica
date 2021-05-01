b38377ac6b2dd473677483d792991c78bf276dd8
王瑞瑜 <roywang@h2o-team.com>
2021-05-01 20:35:16 +0800

    hide <NavigationBar /> if no user is logged in

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import NavigationBar from '../components/NavigationBar'
 import { useUser } from '../lib/hooks'
 
 export default function App ({ Component, pageProps }) {
-  useUser({ redirectTo: '/' })
+  const user = useUser({ redirectTo: '/' })
 
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
-      <NavigationBar />
+      {user ? <NavigationBar /> : null}
       <Component {...pageProps} />
     </div>
   )
