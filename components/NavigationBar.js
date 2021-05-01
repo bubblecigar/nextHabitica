8d6412256b283c4a3e605e2ce331883b4487e464
王瑞瑜 <roywang@h2o-team.com>
2021-05-01 20:05:55 +0800

    remove mobile rwd and add logout button

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -6,7 +6,6 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { useUser } from '../lib/hooks'
 
 const navigation = ['Sleep', 'Eat', 'Exercise']
-const profile = ['Your Profile', 'Settings', 'Sign out']
 
 function classNames (...classes) {
   return classes.filter(Boolean).join(' ')
@@ -36,7 +35,7 @@ const NavigationBar = () => {
                       />
                     </Link>
                   </div>
-                  <div className='hidden md:block'>
+                  <div>
                     <div className='ml-10 flex items-baseline space-x-4'>
                       {navigation.map((item, itemIdx) =>
                         (
@@ -52,7 +51,7 @@ const NavigationBar = () => {
                     </div>
                   </div>
                 </div>
-                <div className='hidden md:block'>
+                <div>
                   <div className='ml-4 flex items-center md:ml-6'>
                     <Menu as='div' className='ml-3 relative'>
                       {({ open }) => (
@@ -81,21 +80,19 @@ const NavigationBar = () => {
                               static
                               className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
                             >
-                              {profile.map((item) => (
-                                <Menu.Item key={item}>
-                                  {({ active }) => (
-                                    <a
-                                      href='#'
-                                      className={classNames(
-                                        active ? 'bg-gray-100' : '',
-                                        'block px-4 py-2 text-sm text-gray-700'
-                                      )}
-                                    >
-                                      {item}
-                                    </a>
-                                  )}
-                                </Menu.Item>
-                              ))}
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href='/api/logout'
+                                    className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block px-4 py-2 text-sm text-gray-700'
+                                    )}
+                                  >
+                                    Log out
+                                  </a>
+                                )}
+                              </Menu.Item>
                             </Menu.Items>
                           </Transition>
                         </>
@@ -103,71 +100,8 @@ const NavigationBar = () => {
                     </Menu>
                   </div>
                 </div>
-                <div className='-mr-2 flex md:hidden'>
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className='bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
-                    <span className='sr-only'>Open main menu</span>
-                    {open ? (
-                      <XIcon className='block h-6 w-6' aria-hidden='true' />
-                    ) : (
-                      <MenuIcon className='block h-6 w-6' aria-hidden='true' />
-                    )}
-                  </Disclosure.Button>
-                </div>
               </div>
             </div>
-
-            <Disclosure.Panel className='md:hidden'>
-              <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                {navigation.map((item, itemIdx) =>
-                  itemIdx === 0 ? (
-                    <Fragment key={item}>
-                      <a href='#' className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>
-                        {item}
-                      </a>
-                    </Fragment>
-                  ) : (
-                    <a
-                      key={item}
-                      href='#'
-                      className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-                    >
-                      {item}
-                    </a>
-                  )
-                )}
-              </div>
-              <div className='pt-4 pb-3 border-t border-gray-700'>
-                <div className='flex items-center px-5'>
-                  <div className='flex-shrink-0'>
-                    <img
-                      className='h-10 w-10 rounded-full'
-                      src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                      alt=''
-                    />
-                  </div>
-                  <div className='ml-3'>
-                    <div className='text-base font-medium leading-none text-white'>Tom Cook</div>
-                    <div className='text-sm font-medium leading-none text-gray-400'>tom@example.com</div>
-                  </div>
-                  <button className='ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
-                    <span className='sr-only'>View notifications</span>
-                    <BellIcon className='h-6 w-6' aria-hidden='true' />
-                  </button>
-                </div>
-                <div className='mt-3 px-2 space-y-1'>
-                  {profile.map((item) => (
-                    <a
-                      key={item}
-                      href='#'
-                      className='block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700'
-                    >
-                      {item}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Disclosure.Panel>
           </>
         )}
       </Disclosure>
