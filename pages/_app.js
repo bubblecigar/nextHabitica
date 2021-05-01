8aed55d21c46e8018fbc7acfaac6c4dd3ccd81f0
王瑞瑜 <roywang@h2o-team.com>
2021-05-01 16:46:08 +0800

    refactor <NavigationBar /> from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,13 @@
 import 'tailwindcss/tailwind.css'
-import Link from 'next/link'
+import NavigationBar from '../components/NavigationBar'
 import { useUser } from '../lib/hooks'
 
 export default function App ({ Component, pageProps }) {
-  const user = useUser({ redirectTo: '/' })
+  useUser({ redirectTo: '/' })
+
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
-      <div className='fixed left-0 top-0'>
-        <Link href='/'>Account</Link>
-        {
-          user
-            ? (
-              <>
-                <Link href='/sleep'>sleep</Link>
-                <Link href='/eat'>eat</Link>
-                <Link href='/exercise'>exercise</Link>
-              </>
-            )
-            : null
-        }
-      </div>
-
+      <NavigationBar />
       <Component {...pageProps} />
     </div>
   )
