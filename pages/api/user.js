53c221f9e273061920f1fc6be52b3e4011a98d7b
王瑞瑜 <roywang@h2o-team.com>
2021-05-03 15:32:20 +0800

    extracting getUserFromLoginSession() from api/user

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,10 +1,15 @@
 import { getLoginSession } from '../../lib/auth'
 import { findUser } from '../../lib/user'
 
+export const getUserFromLoginSession = async (req) => {
+  const session = await getLoginSession(req)
+  const user = (session && (await findUser({ username: session.user_name}))) ?? null
+  return user
+}
+
 export default async function user(req, res) {
   try {
-    const session = await getLoginSession(req)
-    const user = (session && (await findUser({ username: session.user_name}))) ?? null
+    const user = await getUserFromLoginSession(req)
     res.status(200).json({ user })
   } catch (error) {
     res.status(500).end('Authentication token is invalid, please log in')
