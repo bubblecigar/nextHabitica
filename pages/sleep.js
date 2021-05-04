465ebf5ae41553faf45d50085b725db3813b4090
王瑞瑜 <roywang@h2o-team.com>
2021-05-04 22:19:33 +0800

    refactor <SleepEditor /> from pages/sleep

--- pages/sleep.js
+++ pages/sleep.js
@@ -5,6 +5,50 @@ import { format } from 'date-fns-tz'
 import zhTWLocale from 'date-fns/locale/zh-TW'
 import { PlusCircleIcon } from '@heroicons/react/solid'
 
+const SleepEditor = (sl) => {
+  return (
+    <tr key={sl.sleep_id}>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>
+          {
+            sl.start
+              ? format(new Date(sl.start), 'MMMdo cccc', {
+                locale: zhTWLocale
+              })
+              : '-'
+          }
+        </div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>
+          {
+            sl.start
+              ? format(new Date(sl.start), 'HH:mm:ss', {
+                locale: zhTWLocale
+              })
+              : '-'
+          }
+        </div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>
+          {sl.end ? format(new Date(sl.end), 'HH:mm:ss') : '-'}
+        </div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+          {(sl.start && sl.end) ? '?' : '-'}
+        </span>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+        <a href='#' className='text-indigo-600 hover:text-indigo-900'>
+            Edit
+        </a>
+      </td>
+    </tr>
+  )
+}
+
 export default function Sleep (props) {
   const sleep = useSleep()
 
@@ -54,47 +98,7 @@ export default function Sleep (props) {
                 </tr>
               </thead>
               <tbody className='bg-white divide-y divide-gray-200'>
-                {sleep.map((sl) => (
-                  <tr key={sl.sleep_id}>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>
-                        {
-                          sl.start
-                            ? format(new Date(sl.start), 'MMMdo cccc', {
-                              locale: zhTWLocale
-                            })
-                            : '-'
-                        }
-                      </div>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>
-                        {
-                          sl.start
-                            ? format(new Date(sl.start), 'HH:mm:ss', {
-                              locale: zhTWLocale
-                            })
-                            : '-'
-                        }
-                      </div>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>
-                        {sl.end ? format(new Date(sl.end), 'HH:mm:ss') : '-'}
-                      </div>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
-                        {(sl.start && sl.end) ? '?' : '-'}
-                      </span>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
-                      <a href='#' className='text-indigo-600 hover:text-indigo-900'>
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                ))}
+                {sleep.map(sl => SleepEditor(sl))}
                 <tr>
                   <td colSpan='5' className='px-6 py-4 whitespace-nowrap text-sm font-medium hover:bg-gray-50 cursor-pointer hover:text-indigo-500  text-indigo-300' onClick={onCreate}>
                     <PlusCircleIcon className='mx-auto h-5 w-5' aria-hidden='true' />
