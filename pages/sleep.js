10bdb8b945a3cd8e794c7da3ab977324ae86831b
王瑞瑜 <roywang@h2o-team.com>
2021-05-10 17:14:25 +0800

    add width for Total column

--- pages/sleep.js
+++ pages/sleep.js
@@ -339,7 +339,7 @@ export default function Sleep (props) {
                   </th>
                   <th
                     scope='col'
-                    className='bg-gray-50 sticky top-0 px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider'
+                    className='bg-gray-50 sticky top-0 px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider w-48'
                   >
                     Total
                   </th>
