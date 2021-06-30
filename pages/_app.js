c80ff9dc0a047d30a56a5c43d66c44aad403ef03
王瑞瑜 <roywang@h2o-team.com>
2021-06-30 20:54:24 +0800

    add page icon and text

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 import NavigationBar from '../components/NavigationBar'
 import { useUser } from '../lib/hooks'
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }) {
   const user = useUser({ redirectTo: '/' })
 
   return (
     <div className='min-h-screen flex items-start justify-center bg-gray-50 py-28 px-4 sm:px-6 lg:px-8'>
+      <Head>
+        <title>Next Habitica</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/star.png"></link>
+      </Head>
       {user ? <NavigationBar /> : null}
       <Component {...pageProps} />
     </div>
