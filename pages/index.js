047e8a76f8a035ee9ef34fecdf5c8e07d3aad404
王瑞瑜 <roywang@h2o-team.com>
2021-06-30 21:14:49 +0800

    linting

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import { useUser } from '../lib/hooks'
 import AccountForm from '../components/AccountForm'
 
-export default function Home (props) {
+export default function Home(props) {
   const user = useUser()
 
   if (user === undefined) {
     return <div>loading...</div>
   } else if (user === null) {
     return (
-        <AccountForm />
+      <AccountForm />
     )
   } else {
     return (
       <>
-        <div>currently login as { user?.user_name }</div>
-        <a href='/api/logout'>logout</a>
+        <div>currently login as {user.user_name}</div>
       </>
     )
   }
