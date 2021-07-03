0800ea82c49b8b23ad8644d51f6cf2d536bb3b39
王瑞瑜 <roywang@h2o-team.com>
2021-07-03 17:35:51 +0800

    get friend user_name

--- components/friend/FriendList.js
+++ components/friend/FriendList.js
@@ -43,7 +43,7 @@ const FriendRow = ({ friend }) => {
     <tr>
       <td className='bg-indigo-50 px-6 py-4 whitespace-nowrap'>
         <div className='text-sm text-gray-900 text-center'>
-          {friendId}
+          {friend.info.user_name}
         </div>
       </td>
       <td className='bg-indigo-50 px-6 py-4 whitespace-nowrap'>
--- pages/api/friend/read.js
+++ pages/api/friend/read.js
@@ -20,11 +20,42 @@ const readUserFriends = async (userId) => {
   })
 }
 
+
+const readUserInfo = async (userId) => {
+  return new Promise((resolve, reject) => {
+    db.query(`
+      SELECT user_name FROM entries
+      WHERE user_id = $1
+    `
+      , [userId]
+      , (err, dbRes) => {
+        if (err) {
+          console.log('readUserInfo err:', err)
+          reject(err)
+        } else {
+          resolve(dbRes)
+        }
+      })
+  })
+}
+
 export default async function read(req, res) {
   try {
     const user = await getUserFromLoginSession(req)
     const dbRes = await readUserFriends(user.user_id)
-    res.status(200).send(dbRes.rows)
+    const promises = dbRes.rows.map(
+      async row => {
+        if (row.requestor === user.user_id) {
+          const info = await readUserInfo(row.acceptor)
+          return { ...row, info: info.rows[0] }
+        } else {
+          const info = await readUserInfo(row.requestor)
+          return { ...row, info: info.rows[0] }
+        }
+      }
+    )
+    const result = await Promise.all(promises)
+    res.status(200).send(result)
   } catch (error) {
     console.log('api/friend/read error:', error)
     res.status(500).end(error)
